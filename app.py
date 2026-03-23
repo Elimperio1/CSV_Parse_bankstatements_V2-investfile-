14e49aa056fe8e44234fcbd0d2ded06b5e910f3b
Elimperio Accountants <admin@elimperio.co.za>
2026-03-23 15:38:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -179,7 +179,7 @@ div[data-testid="stExpander"] p         { color: #1a2f5e !important; }
     color: #ffffff !important;
 }
 .stDownloadButton > button {
-    background: #a3c4c9 !important;
+    background: ##63eaff !important;
     color: #ffffff !important;
     border: none !important;
     border-radius: 6px !important;
